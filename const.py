e12f1be40baa25f29a666bbb1b54f4f78340ef4b
spacearound404 <viksnamax@mail.ru>
2023-07-19 16:51:09 +0300

    +

---
+++ const.py
@@ -0,0 +1,4 @@
+POSTGRES_URL = "postgresql://postgres:postgres@host.docker.internal:5432/postgres"
+ASYNC_POSTGRES_URL = "postgresql+asyncpg://postgres:postgres@host.docker.internal:5432/postgres"
+WAIT_TIME = 5
+INDEX_COLUMN = 1
